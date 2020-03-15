63e21483d39f6c67e06b8862a75eeb7d11130c00
unknown <coolmos@naver.com>
2020-03-15 11:56:43 +0900

    'axios'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import world_word from "./world.js"
 import css from './style.css'
 import _ from 'lodash'
 import $ from 'jquery'
+import axios from 'axios'
 
 console.log('css ', css)
 // document.querySelector("#root").innerHTML =_.join([hello_word, world_word], ' ');
 $('#root').text('aaa')
+
+axios.get('test.txt')
+.then(function(response){
+    console.log(response.data)
+})
